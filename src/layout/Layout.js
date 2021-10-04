90e35b654ebd51beefe6ca757611a51990f6d7e9
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-04 19:49:00 -0300

    Definido layout a usar

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <p>Hola, mundo!</p>
+            <div>
+                <div className="row">
+                    <div className="col-12 col-lg-4">
+                        <div class="form-group">
+                            <label>Texto:</label>
+                            <input class="form-control"/>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <p>Texto: ?</p>
+                <hr />
+                <button class="btn btn-danger">
+                    <i className="fa fa-times fa-fw"></i> Borrar Texto
+                </button>
+            </div>
         );
     }
 }
